1fa43beea4a137fa528861d83a116f7db1675028
OTAVIO MARTINS FICHO <0201392321024>
2024-09-27 10:14:03 -0300

    #

---
+++ Vogal.js
@@ -0,0 +1,9 @@
+
+var letras = ["a","e","i","o","u"];
+
+var letras = (prompt("Digite uma letra"));
+if(letras == "a" || letras == "e" || letras == "i" || letras == "o" || letras == "u"){
+    console.log("Vogal")
+}else{
+    console.log("Consoante")
+}
